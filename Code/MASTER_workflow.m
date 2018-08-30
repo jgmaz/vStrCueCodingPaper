26420682dbb74888981104b54e8bf434b5f4689e
jgmaz <jim.gmaz@gmail.com>
2018-08-30 15:06:25 -0400

    workflow DATA_ROOT streamline

--- Code/MASTER_workflow.m
+++ Code/MASTER_workflow.m
@@ -11,12 +11,12 @@ addpath(genpath([GITHUB_PATH 'vStrCueCodingPaper\Code'])); % paper repo
 DATA_ROOT = 'C:\data\vStrCueCoding\'; 
 
 %% Create folders for spiking data and analysis variables
-directory = 'E:\vStr-cuecoding\promoted\'; % working directory
-spiking_data = 'spike data'; % folder to save processed spiking data
-analysis_variables = 'analysis files'; % folder to save analysis variables
+directory = DATA_ROOT; % working directory
+spike_data = 'spike data\'; % folder to save processed spiking data
+analysis_files = 'analysis files\'; % folder to save analysis variables
 
-mkdir(directory,spiking_data);
-mkdir(directory,analysis_variables);
+mkdir(directory,spike_data);
+mkdir(directory,analysis_files);
 
 %% Behavior example learning curves [NOT COMPLETED]
 
@@ -24,14 +24,14 @@ mkdir(directory,analysis_variables);
 
 %% Preprocess units (needed for Figures 3,4,5,6,7)
 directory = DATA_ROOT; % working directory
-destination = [DATA_ROOT 'spike data\']; % where to save .mat files
+destination = [DATA_ROOT spike_data]; % where to save .mat files
 PETH_generation = 1; % generating PETHs used for Figures 3,5,6,7-supplement is time consuming. Switch to 0 to bypass this step.
 
 genProcess(directory,destination,PETH_generation);
 
 %% Cue-onset GLM (needed for Figure 4)
-directory = 'E:\vStr-cuecoding\promoted\spike data\'; % working directory
-destination = 'E:\vStr-cuecoding\promoted\analysis files\'; % where to save .mat files
+directory = [DATA_ROOT spike_data]; % working directory
+destination = [DATA_ROOT analysis_files]; % where to save .mat files
 num_Shuffs = 2; % 100 used in paper
 select_Epoch = 1; % 1 = cue-onset only, 2 = nosepoke & outcome, 3 = all time points
 
@@ -51,8 +51,8 @@ which_plot = 2; % 1 = cue-onset GLM in Figure 4A; 2 = cue-onset GLM in Figure 4 
 plotGLM(GLM_window,which_plot);
 
 %% Cue-onset LDA (needed for Figure 4)
-directory = 'E:\vStr-cuecoding\promoted\spike data\'; % working directory
-destination = 'E:\vStr-cuecoding\promoted\analysis files\'; % where to save .mat files
+directory = [DATA_ROOT spike_data]; % working directory
+destination = [DATA_ROOT analysis_files]; % where to save .mat files
 num_Iterations = 2; % number of times to perform 10x cross-validation; 100 used in paper
 num_Shuffs = 2; % 100 used in paper
 
@@ -65,8 +65,8 @@ Class_accuracy = genLDAstats(destination);
 plotLDA(Class_accuracy);
 
 %% Nosepoke & outcome GLM (needed for Figure 7)
-directory = 'E:\vStr-cuecoding\promoted\spike data\'; % working directory
-destination = 'E:\vStr-cuecoding\promoted\analysis files\'; % where to save .mat files
+directory = [DATA_ROOT spike_data]; % working directory
+destination = [DATA_ROOT analysis_files]'; % where to save .mat files
 num_Shuffs = 2; % 100 used in paper
 select_Epoch = 2; % 1 = cue-onset only, 2 = nosepoke & outcome, 3 = all time points
 
@@ -86,9 +86,9 @@ which_plot = 4; % 1 = cue-onset GLM in Figure 4A; 2 = cue-onset GLM in Figure 4 
 plotGLM(GLM_window_NP,which_plot);
 
 %% Recoded coefficients (needed for Figure 4,7)
-spike_directory = 'E:\vStr-cuecoding\promoted\spike data\'; % where spiking data resides
-directory = 'E:\vStr-cuecoding\promoted\analysis files\'; % working directory
-destination = 'E:\vStr-cuecoding\promoted\analysis files\'; % where to save .mat files
+spike_directory = [DATA_ROOT spike_data]; % where spiking data resides
+directory = [DATA_ROOT analysis_files]; % working directory
+destination = [DATA_ROOT analysis_files]; % where to save .mat files
 num_Shuffs = 2; % 100 used in paper
 
 GLM_coeff = genRecode(spike_directory,directory,destination);
@@ -111,7 +111,7 @@ which_plot = 3; % 1 = cue-onset correlation matrix in Figure 4D; 2 = cue feature
 plotRecode(GLM_coeff,which_plot);
 
 %% Plot cue-onset examples
-directory = 'E:\vStr-cuecoding\promoted\spike data\'; % where spiking data resides
+directory = [DATA_ROOT spike_data]; % where spiking data resides
 
 % Figure 3
 plotExamples(directory)
@@ -120,7 +120,7 @@ plotExamples(directory)
 plotExamplesSUPP(directory)
 
 %% Plot nosepoke examples
-directory = 'E:\vStr-cuecoding\promoted\spike data\'; % where spiking data resides
+directory = [DATA_ROOT spike_data]; % where spiking data resides
 
 % Figure 6
 plotExamplesNP(directory)
@@ -129,8 +129,8 @@ plotExamplesNP(directory)
 plotExamplesNPSUPP(directory)
 
 %% Cue-onset distributed firing (needed for Figure 5)
-directory = 'E:\vStr-cuecoding\promoted\spike data\'; % where spiking data resides
-destination = 'E:\vStr-cuecoding\promoted\analysis files\'; % where to save .mat files
+directory = [DATA_ROOT spike_data]; % where spiking data resides
+destination = [DATA_ROOT analysis_files]; % where to save .mat files
 cfg.start = 4001; % start time of analysis (ms); centered on 5000 ms
 cfg.end = 6000; % end time of analysis (ms); centered on 5000 ms
 direction = 1; % order by maximum or minimum firing (1 = max, 2 = min)
